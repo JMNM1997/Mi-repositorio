8097b93f52c8f046918155cc67793211522d6985
JMNM1997 <55831761+JMNM1997@users.noreply.github.com>
2020-02-20 21:32:39 +0100

    ok

---
+++ UD04/Canvas1/04.03/js.js
@@ -0,0 +1,34 @@
+var contador=0;
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    
+    
+
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    if (data=="drag3")
+    {
+      contador--
+    }
+
+    else
+    {
+      contador++
+    }
+
+    var p = document.getElementById("num");
+
+
+    p.innerHTML=contador;
+
+  }
